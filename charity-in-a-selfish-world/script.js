75a5183f9c65286b1592d35af0c473d0113ba40f
Ibrahim Al Hazwani <31704631+ibrahimalhazwani@users.noreply.github.com>
2020-05-15 18:10:55 +0200

    animation landing page

---
+++ charity-in-a-selfish-world/script.js
@@ -0,0 +1,42 @@
+$(document).ready(function(){
+    animateDiv(".blue_square");
+    animateDiv(".seagreen_square");
+    animateDiv(".salmon_square");
+    animateDiv(".turquoise_square");
+    animateDiv(".blueviolet_square");
+
+    animateDiv(".red_circle");
+    animateDiv(".coral_circle");
+    animateDiv(".pink_circle");
+    animateDiv(".grey_circle");
+    animateDiv(".lightblue_circle");
+
+    animateDiv(".orange_triangle");
+    animateDiv(".green_triangle");
+    animateDiv(".yellow_triangle");
+    animateDiv(".crimson_triangle");
+
+    animateDiv(".green_rectangle");
+    animateDiv(".burlywood_rectangle");
+    animateDiv(".purple_rectangle");
+    animateDiv(".deepink_rectangle");
+});
+
+function makeNewPosition(){
+    
+    var h = $(window).height() - 100;
+    var w = $(window).width() - 100;
+    
+    var nh = Math.floor(Math.random() * h);
+    var nw = Math.floor(Math.random() * w);
+    
+    return [nh,nw];    
+    
+}
+
+function animateDiv(myclass){
+    var newq = makeNewPosition();
+    $(myclass).animate({top: newq[0], left: newq[1]}, 2000,   function(){
+      animateDiv(myclass);        
+    });
+}; 
