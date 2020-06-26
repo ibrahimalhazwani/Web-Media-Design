5a43fdf9d1c6ebb48b47a65a0ed991646c64c650
Ibrahim Al Hazwani <31704631+ibrahimalhazwani@users.noreply.github.com>
2020-06-26 19:36:55 +0200

    fix-scrollmagic-issue

--- charity-in-a-selfish-world/script.js
+++ charity-in-a-selfish-world/script.js
@@ -30,10 +30,7 @@ $(document).ready(function(){
         "./images/panaro-cut-one.png",
         "./images/panaro-cut-two.png",
         "./images/panaro-cut-three.png",
-        "./images/panaro-empty.png"
-    ];
-
-    var images2 = [
+        "./images/panaro-empty.png",
         "./images/panaro-corda-sottile-1.png",
         "./images/panaro-corda-sottile-2.png",
         "./images/panaro-corda-rotta-1.png",
@@ -44,12 +41,12 @@ $(document).ready(function(){
         "./images/panaro-rotto-corda-5.png",
         "./images/panaro-rotto-corda-6.png"
     ];
-
+    
     // TweenMax can tween any property of any object. We use this object to cycle through the array
     var obj = {curImg: 0};
 
     // create tween
-    var tween = TweenMax.to(obj, 0.5,
+    var tween = TweenMax.to(obj, 1.5,
         {
             curImg: images.length - 1,	        // animate propery curImg to number of images
             roundProps: "curImg",				// only integers so it can be used as an array index
@@ -66,7 +63,7 @@ $(document).ready(function(){
     var controller = new ScrollMagic.Controller();
 
     // build scene
-    var scene = new ScrollMagic.Scene({triggerElement: ".div-title-trigger", duration: 2000})
+    var scene = new ScrollMagic.Scene({triggerElement: ".div-title-trigger", duration: 4050})
                     .setTween(tween)
                     .addIndicators() // add indicators (requires plugin)
                     .addTo(controller);
@@ -75,37 +72,6 @@ $(document).ready(function(){
     $("form.move input[name=duration]:radio").change(function () {
         scene.duration($(this).val());
     });
-
-    // TweenMax can tween any property of any object. We use this object to cycle through the array
-    var obj2 = {curImg: 0};
-
-    // create tween
-    var tween2 = TweenMax.to(obj2, 0.5,
-        {
-            curImg: images2.length - 1,	        // animate propery curImg to number of images
-            roundProps: "curImg",				// only integers so it can be used as an array index
-            immediateRender: true,			    // load first image automatically
-            ease: Linear.easeNone,			    // show every image the same ammount of time
-            onUpdate: function () {
-                console.log(images2[obj2.curImg])
-                $("#myimg").attr("src", images2[obj2.curImg]); // set the image source
-            }
-        }
-    );
-
-    // build scene
-    var scene2 = new ScrollMagic.Scene({triggerElement: ".div-title-trigger-2", duration: 1200})
-                    .setTween(tween2)
-                    .addIndicators() // add indicators (requires plugin)
-                    .addTo(controller);
-
-    // handle form change
-    $("form.move input[name=duration]:radio").change(function () {
-        scene2.duration($(this).val());
-    });
-
-
-
 });
 
 function makeNewPosition(){
